5ae6c95a0cd75e25e126adb8286c090f936eec9d
Rafael Soares <rafaelsoaresbr@gmail.com>
2016-04-07 14:07:22 -0400

    Fix inst_pacaur script

--- devel/inst_pacaur.sh
+++ devel/inst_pacaur.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 # Install cower
 cd /home/guest
-curl https://aur.archlinux.org/cgit/aur.git/snapshot/cower.tar.gz | tar -xzC .
+curl https://aur.archlinux.org/cgit/aur.git/snapshot/cower.tar.gz | tar -xz
 gpg --recv-keys --keyserver hkp://pgp.mit.edu 1EB2638FF56C0C53
+cd /home/guest/cower
 makepkg -sci --asdeps --noprogressbar --noconfirm
 # Install pacaur
 cd /home/guest
-curl https://aur.archlinux.org/cgit/aur.git/snapshot/cower.tar.gz | tar -xzC .
+curl https://aur.archlinux.org/cgit/aur.git/snapshot/pacaur.tar.gz | tar -xz
+cd /home/guest/pacaur
 makepkg -sci --noprogressbar --noconfirm
 # cleanup
 rm -rf /home/guest/cower/*
