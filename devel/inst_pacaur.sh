246cbc88fe31b338aafef3a42e75e64bc0427a38
Rafael Soares <rafaelsoaresbr@gmail.com>
2016-04-07 12:58:01 -0400

    Optimize archlinux-devel Dockerfile

--- devel/inst_pacaur.sh
+++ devel/inst_pacaur.sh
@@ -1,23 +1,17 @@
 #!/usr/bin/env bash
 # Install cower
 cd /home/guest
-git clone https://aur.archlinux.org/cower.git
-cd cower
+curl https://aur.archlinux.org/cgit/aur.git/snapshot/cower.tar.gz | tar -xzC .
 gpg --recv-keys --keyserver hkp://pgp.mit.edu 1EB2638FF56C0C53
 makepkg -sci --asdeps --noprogressbar --noconfirm
 # Install pacaur
 cd /home/guest
-git clone https://aur.archlinux.org/pacaur.git
-cd pacaur
+curl https://aur.archlinux.org/cgit/aur.git/snapshot/cower.tar.gz | tar -xzC .
 makepkg -sci --noprogressbar --noconfirm
 # cleanup
 rm -rf /home/guest/cower/*
-rm -rf /home/guest/cower/.git/*
 rm -rf /home/guest/cower/.*
-rmdir /home/guest/cower/.git
 rmdir /home/guest/cower
 rm -rf /home/guest/pacaur/*
-rm -rf /home/guest/pacaur/.git/*
 rm -rf /home/guest/pacaur/.*
-rmdir /home/guest/pacaur/.git
 rmdir /home/guest/pacaur
